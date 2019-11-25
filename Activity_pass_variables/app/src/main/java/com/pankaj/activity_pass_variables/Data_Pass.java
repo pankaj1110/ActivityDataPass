809e17bc4a6c03e6045aefc39e5413f77c676093
Pankaj <pankajchavan013@gmail.com>
2019-11-25 08:41:01 +0530

    clash free time table

---
+++ Activity_pass_variables/app/src/main/java/com/pankaj/activity_pass_variables/Data_Pass.java
@@ -0,0 +1,25 @@
+package com.pankaj.activity_pass_variables;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Data_Pass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data__pass);
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(MainActivity.EXTRA_TEXT);
+        int number = intent.getIntExtra(MainActivity.EXTRA_NUMBER, 0);
+
+        TextView textView1 = (TextView) findViewById(R.id.textview1);
+        TextView textView2 = (TextView) findViewById(R.id.textview2);
+
+        textView1.setText(text);
+        textView2.setText("" + number);
+    }
+}
