e19b8f0b9655927d6085e114765cc8d700927841
Pankaj <pankajchavan013@gmail.com>
2019-11-25 08:27:45 +0530

    clash free time table

---
+++ Activity_Data_Pass/app/src/main/java/com/pankaj/activity_data_pass/Display_Activity.java
@@ -0,0 +1,13 @@
+package com.pankaj.activity_data_pass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Display_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_);
+    }
+}
